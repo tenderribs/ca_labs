20bb542026a6225054e27da3f417175c641c9b14
mark <mark@Mark-MBP>
2025-11-20 15:47:58 +0100

    init commit

---
+++ lab4/vcpkg/ports/cachelib/portfile.cmake
@@ -0,0 +1,40 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO facebook/CacheLib
+    REF "v${VERSION}"
+    SHA512 5670b796bad9bca30b126cfff9466de095294f855598c663193054b08cace03f0ba73677735ffc26f9a920d58eec9430b83ad7f7187dbcba9264d7e3635ed30e
+    HEAD_REF main
+    PATCHES
+        fix-build.patch
+        fix-glog.patch
+        fix-fmt.patch
+)
+
+FIND_PATH(NUMA_INCLUDE_DIR NAME numa.h
+    PATHS ENV NUMA_ROOT
+    HINTS "$ENV{HOME}/local/include" /opt/local/include /usr/local/include /usr/include
+)
+
+IF (NOT NUMA_INCLUDE_DIR)
+    MESSAGE(FATAL_ERROR "Numa library not found.\nTry: 'sudo yum install numactl numactl-devel' (or sudo apt-get install libnuma1 libnuma-dev)")
+ENDIF ()
+
+file(REMOVE "${SOURCE_PATH}/cmake/FindGlog.cmake")
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}/cachelib"
+    OPTIONS
+        -DBUILD_TESTS=OFF
+        -DCMAKE_INSTALL_DIR=share/cachelib
+)
+
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(CONFIG_PATH share/cachelib PACKAGE_NAME cachelib)
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
+endif()
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
+
+vcpkg_fixup_pkgconfig()
---
+++ lab4/vcpkg/ports/libnick/portfile.cmake
@@ -0,0 +1,32 @@
+if(VCPKG_TARGET_IS_WINDOWS)
+    vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+endif()
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO NickvisionApps/libnick
+    REF "${VERSION}"
+    SHA512 fbee410b7b470ff98a92c8c56ed8698b1912894e336d1b07376acbae787b88e48374b3c80da0ec13692139f22b89208de7cc2787a6563a3e73c7412069bdb285
+    HEAD_REF main
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DBUILD_TESTING=OFF
+)
+
+vcpkg_cmake_install()
+
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/libnick)
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+
+vcpkg_copy_pdbs()
+
+vcpkg_fixup_pkgconfig()
+
+file(INSTALL "${SOURCE_PATH}/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+
+configure_file("${CMAKE_CURRENT_LIST_DIR}/usage" "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage" COPYONLY)
